26ba5e83e9321c8799308755646605ea1b33a06a
Paul van der Ende <pvanderende@xebialabs.com>
2019-12-19 08:30:41 +0100

    adding templates

---
+++ fragments/docker-repository/docker-repository.py
@@ -0,0 +1,12 @@
+def stitch(ctx, source):
+
+    nodes = result.get("services")
+
+    if nodes:
+        for n in nodes:
+            image = n.get("image")
+            if image:
+                s = image.textValue()
+                n.put("image", "registry-1.docker.io/%s" % s)
+
+    return source
